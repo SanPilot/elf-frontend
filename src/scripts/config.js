ca096047cf044dddef4ba2cfa4518664b1449669
Hussain Khalil <hussainpilot05@gmail.com>
2017-06-06 18:33:22 -0400

    Add new config options

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -3,6 +3,10 @@ Frontend configuration options
 */
 
 var config = {
+  apiAddress: "ws://127.0.0.1:3000", // network address of backend server
   debug: true, // print debug statements to console
-  featureCheck: true, // ensure client supports all functionality 
+  featureCheck: true, // ensure client supports all functionality
+  retries: 5, // when failed, how many times to retry request
+  serverTimeout: 10000, // how long to wait for a response
+  lastSec: 2 // backend rate-limiting
 };
